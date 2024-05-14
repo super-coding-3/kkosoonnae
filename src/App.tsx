e6753e49d6e65f0c13200c138f70b6173b9ac01a
woohyuk Park <psj4415@naver.com>
2024-05-14 16:13:03 +0900

    [feature]Signup page create, PageTitle Components create

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./page/Main";
+import Signup from "./page/Signup";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </div>
   );
