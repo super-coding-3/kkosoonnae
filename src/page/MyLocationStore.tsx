3869e5121d4288829f5152e75964df6baa7bf710
woohyuk Park <psj4415@naver.com>
2024-05-15 19:28:09 +0900

    [feature]KaKaoMap 가져오기 완료

--- src/page/MyLocationStore.tsx
+++ src/page/MyLocationStore.tsx
@@ -3,11 +3,13 @@ import OuterLayout from '../components/common/OuterLayout';
 import PageTitle from '../components/common/PageTitle';
 import Footer from '../components/common/Footer';
 import Nav from '../components/common/Nav';
+import KakaoMap from '../components/mylocationstore/KakaoMap';
 
 const MyLocationStore = () => {
     return (
         <OuterLayout>
             <PageTitle title="내 주변 미용실"/>
+            <KakaoMap/>
             <Footer/>
             <Nav/>
         </OuterLayout>
