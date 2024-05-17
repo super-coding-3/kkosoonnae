6ec3ad45711e6d9884098d6cc9cc930b25f69968
woohyuk Park <psj4415@naver.com>
2024-05-17 12:46:46 +0900

    [fix]mater 병합

--- src/components/mylocationstore/KakaoMap.tsx
+++ src/components/mylocationstore/KakaoMap.tsx
@@ -3,23 +3,26 @@ import styled from "styled-components";
 
 declare global {
   interface Window {
-      kakao: any;
+    kakao: any;
   }
   const kakao: any;
 }
 
 const KakaoMap: React.FC = () => {
   useEffect(() => {
-    const container = document.getElementById('map');
+    const container = document.getElementById("map");
     if (container && window.kakao && window.kakao.maps) {
       const options = {
         center: new window.kakao.maps.LatLng(33.450701, 126.570667),
         level: 3,
       };
       const map = new window.kakao.maps.Map(container, options);
-      
+
       // 예시 마커 추가 (원하는 경우)
-      const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+      const markerPosition = new window.kakao.maps.LatLng(
+        33.450701,
+        126.570667
+      );
       const marker = new window.kakao.maps.Marker({
         position: markerPosition,
       });
@@ -29,7 +32,6 @@ const KakaoMap: React.FC = () => {
 
   return <MapDiv id="map" />;
 };
-
 
 export default KakaoMap;
 const MapDiv = styled.div`
