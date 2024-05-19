7ef1dd66d1f0c550fafa391eb0baeef2d0a158a4
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-05-19 19:56:36 +0900

    [feature] Baseurl 공통 작업 / 예약하기-불러오기 모달

--- api/customAxios.ts
+++ api/customAxios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 //여기에 서버 주소기입
-const BASE_URL = "test";
+const BASE_URL = "http://43.200.162.185:8080/";
 
 const HttpClient: AxiosInstance = axios.create({
   baseURL: BASE_URL, // 기본 서버 주소 입력
