b6dabd84ccd16ea776a2c19424fefb7d1bc0d091
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-05-21 21:10:57 +0900

    [setting] 전역상수 파일, 라우터 파일 분리, api 파일 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
