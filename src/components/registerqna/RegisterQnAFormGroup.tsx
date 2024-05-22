22d36b9b58539ce6b9d4af05f0ae5d2a7a37acde
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-05-23 03:04:59 +0900

    [feature] 문의하기 페이지 API 50% 구현, 문의 완료 모달을 페이지로 변경

---
+++ src/components/registerqna/RegisterQnAFormGroup.tsx
@@ -0,0 +1,33 @@
+import { ErrorMessage, Field } from "formik";
+import React from "react";
+
+interface RegisterQnAFormGroupProps {
+  label: string;
+  component: string;
+  name: string;
+  placeholder: string;
+  height: number;
+}
+
+const RegisterQnAFormGroup: React.FC<RegisterQnAFormGroupProps> = (props) => {
+  return (
+    <div className="flex flex-col gap-2 mt-5">
+      <label className="font-bold">{props.label}</label>
+      <Field
+        type="text"
+        component={props.component}
+        className={`h-${props.height} p-2.5 w-full border-2 border-gray-300 appearance-none focus:border-MAIN_COLOR focus:outline-none focus:ring-transparent rounded-lg align-text-top`}
+        name={props.name}
+        placeholder={props.placeholder}
+        required
+      />
+      <ErrorMessage
+        name={props.name}
+        component="div"
+        className="text-rose-300"
+      />
+    </div>
+  );
+};
+
+export default RegisterQnAFormGroup;
