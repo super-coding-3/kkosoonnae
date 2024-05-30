ff54613d526042d35ac956dcb96c5d25fab2ba65
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-05-30 19:31:01 +0900

    [fix] footer 정리 및 mailto 연결

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,22 +2,19 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="py-4 px-4">
-      <p className="text-xs text-center break-keep text-slate-400 leading-4">
-        대표자명 : 원종석, 권규석 사업자 등록번호 : 120-86-08810 주소 : 서울
-        강남구 봉은사로427, 9층 통신판매 신고번호 : 2021-서울강남-02122 전화번호
-        : 1644-0579 이메일 주소: wyatt1@kakaocorp.com 호스팅 서비스 제공자 :
-        (주)카카오
-      </p>
+    <footer className="py-4 px-4">
       <p className="text-xs text-center break-keep text-slate-400 leading-4 mt-3">
-        (주)와이어트는 통신판매중개자로서 카카오헤어샵의 거래당사자가 아니며,
-        입점판매자가 등록한 상품정보 및 거래에 대해 책임을 지지 않습니다.
-        (주)와이어트가 소유/운영/관리하는 웹사이트 및 앱 내 예약정보를 포함한
-        모든정보,UI, 콘텐츠 등에 대한 무단 복제, 배포, 방송 또는 전송, 스크래핑
-        등의 행위는 관련 법령에 의하여 엄격히 금지 됩니다. 콘텐츠산업 진흥법에
-        따른 표시 © Wyatt
+        꼬순내는 반려동물 주인들과 전문 반려동물 미용사를 연결하는 플랫폼입니다.
+        저희는 사용자들에게 편리하고 원활한 경험을 제공하기 위해 노력하고
+        있습니다. (주)꼬순내가 소유/운영/관리하는 웹사이트 및 앱 내 예약정보를
+        포함한 모든정보,UI, 콘텐츠 등에 대한 무단 복제, 배포, 방송 또는 전송,
+        스크래핑 등의 행위는 관련 법령에 의하여 엄격히 금지 됩니다. 콘텐츠산업
+        진흥법에 따른 표시 © Kkosoonae 문의하기: 전화번호: 1234-0777
+        <a href="mailto:support@kkosoonae.com">support@kkosoonae.com</a>
+        <address>주소:서울특별시 강남구 봉은사로 427</address>
+        &copy; 2024 꼬순내. All rights reserved.
       </p>
-    </div>
+    </footer>
   );
 };
 
