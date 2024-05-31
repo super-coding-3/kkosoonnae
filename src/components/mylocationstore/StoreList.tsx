5c893e9dbc38cd18f3daac6751a5f9dd8c630d6a
woohyuk Park <psj4415@naver.com>
2024-05-31 16:02:17 +0900

    [fix]MylocationStore page 리스트 css 수정

--- src/components/mylocationstore/StoreList.tsx
+++ src/components/mylocationstore/StoreList.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+
 import { List, Avatar } from "flowbite-react";
-import { Store } from "./LocationApi";
 import { Link } from "react-router-dom";
 
 
-import React from "react";
+interface Store {
+  storeNo: number;
+  storeName: string;
+  latestReviewMessage: string;
+  averageReviewScore: number;
+}
 
 interface StoreListProps {
   stores: Store[];
@@ -13,7 +19,7 @@ const StoreList: React.FC<StoreListProps> = ({ stores }) => {
   return (
     <>
       <h1 className="text-xl mb-4 mt-4 font-semibold">내 주변 매장 최근 리뷰</h1>
-      <List className="w-full divide-y divide-gray-200 bg-white rounded-lg">
+      <List style={{ listStyleType: 'none'}} className="w-full divide-y divide-gray-200 bg-white rounded-lg">
         {stores.map((store) => (
           <List.Item key={store.storeNo} className="py-2">
             <div className="flex items-center space-x-4">
@@ -21,25 +27,26 @@ const StoreList: React.FC<StoreListProps> = ({ stores }) => {
                 img="/img/common/icon-dog_haircut.svg"
                 alt={`${store.storeName} image`}
                 rounded
-                size="sm"
-                className="w-13 h-13"
+                size="lg"
+                className="w-14 h-14"
               />
-              <div className="ml-4 flex-1">
+              <div className="ml-4 flex-1 min-w-0">
                 <p className="text-lg font-semibold">
                   <Link to={`/salon/${store.storeNo}`}>{store.storeName}</Link>
                 </p>
-                <p className="text-sm mt-1 text-gray-600 truncate">
-                  <span className="font-semibold mb-1">매장 내 최근 리뷰</span>
-                  "{store.latestReviewMessage}"
-                </p>
+                <div className="text-sm mt-1 text-gray-600 truncate">
+                  <span className="font-semibold mb-1 block">매장 내 최근 리뷰</span>
+                  <span className="mt-2 text-[14px] font-semibold block whitespace-normal">"{store.latestReviewMessage}"</span>
+                </div>
               </div>
-              <div className="flex items-center text-base font-bold text-gray-700">
-                <img src="/img/common/icon-star.svg" className="w-4 h-4 mr-1" />
+              <div className="flex items-center text-base font-bold text-gray-700 flex-shrink-0 whitespace-nowrap">
+                <img src="/img/common/icon-star.svg" className="w-4 h-4" />
                 {store.averageReviewScore}점
               </div>
             </div>
           </List.Item>
         ))}
+      
       </List>
     </>
   );
