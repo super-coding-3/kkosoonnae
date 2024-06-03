cb8bd96b560e8852790239c1726a57937b8c3c42
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-06-03 12:10:12 +0900

    [hotfix] 효경 작업 + 메인 엔드포인트 변경 / 예약페이지 수정

--- src/components/reservation/ReservationForm.tsx
+++ src/components/reservation/ReservationForm.tsx
@@ -112,7 +112,7 @@ const ReservationForm: React.FC<ReservationFormProps> = ({
 
           <Formik
             initialValues={initialValues}
-            // validationSchema={reservationSchema}
+            validationSchema={reservationSchema}
             onSubmit={(values) => reservationHandleSubmit(values)}
           >
             {({ setFieldValue }) => (
--- src/schema/formSchema.tsx
+++ src/schema/formSchema.tsx
@@ -1,6 +1,7 @@
 import * as Yup from "yup";
 
 const reservationSchema = Yup.object().shape({
+  storeNumber: Yup.number(),
   salonName: Yup.string().required("업체명을 입력해주세요."),
   reservationDate: Yup.string().required("예약일자를 선택해주세요."),
   reservationTime: Yup.string().required("예약시간을 선택해주세요."),
@@ -29,29 +30,29 @@ const SignupSchema = Yup.object().shape({
     )
     .required("아이디는 필수입니다"),
 
-    password: Yup.string()
+  password: Yup.string()
     .matches(
       SignUpPasswordRegex,
       "비밀번호는 최소 4자리이상 대문자와 숫자를 포함하여야 합니다"
     )
     .required("비밀번호는 필수입니다"),
-  
-    passwordCheck: Yup.string()
+
+  passwordCheck: Yup.string()
     .oneOf([Yup.ref("password"), undefined], "비밀번호가 일치하지 않습니다")
     .required("비밀번호 확인은 필수입니다."),
-    email: Yup.string()
+  email: Yup.string()
     .email("올바른 이메일 형식을 입력하세요")
     .required("이메일은 필수입니다."),
-    phone: Yup.string()
+  phone: Yup.string()
     .matches(
       phoneRegex,
       "전화번호 형식에 맞지 않습니다. 000-0000-0000 형식으로 입력해주세요."
     )
     .required("핸드폰번호는 필수입니다."),
-    nickName: Yup.string().required("닉네임은 필수입니다."),
-    zipCode: Yup.string().required("우편번호는 필수입니다."),
-    address: Yup.string().required("기본주소는 필수입니다."),
-    addressDtl: Yup.string().required("상세주소는 필수입니다."),
+  nickName: Yup.string().required("닉네임은 필수입니다."),
+  zipCode: Yup.string().required("우편번호는 필수입니다."),
+  address: Yup.string().required("기본주소는 필수입니다."),
+  addressDtl: Yup.string().required("상세주소는 필수입니다."),
 });
 
 const LoginSchema = Yup.object().shape({
