fbc4c22a5d4bdfd664cd421efe10e554bdeb0350
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-06-13 13:59:29 +0900

    [feature] 등록펫 없을시 분기처리

--- src/components/reservation/MyKkosoonaeModal.tsx
+++ src/components/reservation/MyKkosoonaeModal.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Avatar } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
+
 import HttpClient from "../../utils/api/customAxios";
+import SubmitBtn from "../common/SubmitBtn";
 
 interface MyKkosoonaeModalProps {
   openModal: boolean;
@@ -22,14 +25,17 @@ const MyKkosoonaeModal: React.FC<MyKkosoonaeModalProps> = ({
   onPetSelect,
 }) => {
   const [petInfo, setPetInfo] = useState<MypetInfo[]>([]);
+  const navigate = useNavigate();
 
   const getPetInfo = async () => {
-    const { data } = await HttpClient.get<MypetInfo[]>(
-      "api/reservation/my-pet"
-    );
-    setPetInfo(data);
-    console.log(data);
-    return data;
+    try {
+      const { data } = await HttpClient.get<MypetInfo[]>(
+        "api/reservation/my-pet"
+      );
+      setPetInfo(data);
+    } catch (error) {
+      setPetInfo([]);
+    }
   };
 
   useEffect(() => {
@@ -48,32 +54,50 @@ const MyKkosoonaeModal: React.FC<MyKkosoonaeModalProps> = ({
       <Modal show={openModal} onClose={() => setOpenModal(false)} size="md">
         <Modal.Header>내 꼬순내 정보 불러오기</Modal.Header>
         <Modal.Body>
-          <div>
-            {petInfo.map((pet, index) => (
-              <div
-                key={index}
-                className="border border-gray-400 rounded mb-2 flex items-center gap-4 px-2 py-2"
-              >
-                <Avatar img={pet.petImg} size="lg" alt={pet.petName} rounded />
-                <div className="w-32">
-                  <h3 className="text-lg font-bold">{pet.petName}</h3>
-                  <p className="text-xs text-gray-500 to-MAIN_COLOR">
-                    견종/묘종 {pet.breed}
-                  </p>
-
-                  <p className="text-xs text-gray-500">
-                    몸무게: {pet.weight} kg
-                  </p>
-                </div>
-                <button
-                  className="h-10 border border-gray-800 rounded text-xs text-gray-800 px-4 py-2 ml-auto"
-                  onClick={() => handlePetSelect(pet)}
+          {petInfo.length > 0 ? (
+            <div>
+              {petInfo.map((pet, index) => (
+                <div
+                  key={index}
+                  className="border border-gray-400 rounded mb-2 flex items-center gap-4 px-2 py-2"
                 >
-                  선택하기
-                </button>
-              </div>
-            ))}
-          </div>
+                  <Avatar
+                    img={pet.petImg}
+                    size="lg"
+                    alt={pet.petName}
+                    rounded
+                  />
+                  <div className="w-32">
+                    <h3 className="text-lg font-bold">{pet.petName}</h3>
+                    <p className="text-xs text-gray-500 to-MAIN_COLOR">
+                      견종/묘종 {pet.breed}
+                    </p>
+
+                    <p className="text-xs text-gray-500">
+                      몸무게: {pet.weight} kg
+                    </p>
+                  </div>
+                  <button
+                    className="h-10 border border-gray-800 rounded text-xs text-gray-800 px-4 py-2 ml-auto"
+                    onClick={() => handlePetSelect(pet)}
+                  >
+                    선택하기
+                  </button>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="text-center">
+              <p>등록된 내 꼬순내 정보가 없습니다.</p>
+              <button
+                type="button"
+                onClick={() => navigate("/mypage")}
+                className="w-full my-4 py-2 text-white	 bg-MAIN_COLOR rounded"
+              >
+                내 꼬순내(펫) 등록하기
+              </button>
+            </div>
+          )}
         </Modal.Body>
       </Modal>
     </div>
