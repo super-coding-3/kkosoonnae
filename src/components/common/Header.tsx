9797deef663f266c27cb0db9d9c63e917c40d407
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-06-17 19:11:25 +0900

    [style] Header styled components 삭제

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styled from "styled-components";
+import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
 import SearchModal from "../search/SearchModal";
@@ -16,45 +16,19 @@ const Header: React.FC = () => {
   };
 
   return (
-    <HeaderWrap>
+    <div className="max-w-[640px] h-14 mx-auto my-0 bg-white shadow px-4 fixed top-0 left-0 right-0 z-10 flex justify-center items-center">
       <Link to="/">
         <img src="img/logo.svg" alt="꼬순내 로고" />
       </Link>
-      <BtnHeaderSearch onClick={handleButtonClick}>
-        <img src="img/nav/icon-search.svg" alt="통합검색 아이콘" />
-      </BtnHeaderSearch>
+      <button
+        onClick={handleButtonClick}
+        className="w-[55px] h-14 absolute top-0 right-0"
+      >
+        <CiSearch size={30} />
+      </button>
       {isModalOpen && <SearchModal onClose={handleModalClose} />}
-    </HeaderWrap>
+    </div>
   );
 };
-
-const HeaderWrap = styled.header`
-  max-width: 640px;
-  min-width: 375px;
-  height: 55px;
-  margin: 0 auto;
-  background: #fff;
-  box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.12);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 0.5rem;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 5;
-`;
-
-const BtnHeaderSearch = styled.button`
-  width: 55px;
-  height: 55px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  img {
-    display: inline-block;
-  }
-`;
 
 export default Header;
