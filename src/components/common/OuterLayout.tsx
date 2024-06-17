36491c6532bad51ded4483db19032118d481fa1e
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-06-17 19:43:19 +0900

    [style] outerLayout  styled components 제거

--- src/components/common/OuterLayout.tsx
+++ src/components/common/OuterLayout.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import styled from "styled-components";
 
 interface OuterLayoutProps {
   children: React.ReactNode;
 }
 
 const OuterLayout: React.FC<OuterLayoutProps> = ({ children }) => {
-  return <OuterLayoutWrap>{children}</OuterLayoutWrap>;
+  return (
+    <section className="min-h-full max-w-[640px] relative mx-auto bg-white border border-gray-200">
+      {children}
+    </section>
+  );
 };
-
-const OuterLayoutWrap = styled.div`
-  max-width: 640px;
-  min-width: 375px;
-  position: relative;
-  margin: 0 auto;
-  background: #fff;
-  border: #888;
-  min-height: 100vh;
-`;
 
 export default OuterLayout;
