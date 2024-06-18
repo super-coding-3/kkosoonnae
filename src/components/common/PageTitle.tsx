23d91727856c6f57a52f2c6a54e302afef0cb987
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-06-18 18:25:02 +0900

    [style] 페이지 타이틀 styled components 제거

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styled from "styled-components";
 
 interface pageTitleProps {
   title: React.ReactNode;
@@ -33,19 +32,3 @@ const PageTitle: React.FC<pageTitleProps> = (props) => {
 };
 
 export default PageTitle;
-
-const TitleDiv = styled.nav`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  position: sticky;
-  top: 0;
-  align-items: center;
-  padding: 10px 6px;
-  height: 55px;
-  font-size: 20px;
-  font-weight: bold;
-  box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.12);
-  background: #fff;
-  z-index: 5;
-`;
